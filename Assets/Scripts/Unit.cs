66d4122345078601d0bfb7a53e97f3a4ed0ca52f
beamchristian <ckbeamcoding@gmail.com>
2022-08-30 13:20:21 -0400

    unit animations change logic functioning

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+
+    [SerializeField] private Animator unitAnimator;
     private Vector3 targetPosition;
 
     private void Update() 
@@ -14,9 +16,18 @@ public class Unit : MonoBehaviour
             Vector3 moveDirection = (targetPosition - transform.position).normalized;
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime;
+
+            float rotateSpeed = 10f;
+            transform.forward = Vector3.Lerp(transform.forward, moveDirection, Time.deltaTime * rotateSpeed);
+
+            unitAnimator.SetBool("IsWalking", true);
+        }
+        else
+        {
+            unitAnimator.SetBool("IsWalking", false);
         }
 
-        if (Input.GetMouseButtonDown(0)) 
+        if (Input.GetMouseButtonDown(0))  
         {
             Move(MouseWorld.GetPosition());
         }
